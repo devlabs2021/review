532b0a4f7619b53db120c2832802119b2dcb1c9d
Vova Levchenko <Volodymyr.Levchenko@nordstrom.com>
2021-01-02 18:35:54 +0200

    Update

--- cron-jobs/nodejs/1/index.js
+++ cron-jobs/nodejs/1/index.js
@@ -1,4 +1,11 @@
 const jobClient = require('./jobsClient')
+
+const REDIS_HOST = process.env.REDIS_HOST
+const REDIS_PORT = process.env.REDIS_PORT
+const redisConfig = { redis: { port: REDIS_PORT, host: REDIS_HOST } }
+
+const jobClient = new JobsClient(redisConfig)
+
 const { JOB_1, JOB_2 } = require('./constants')
 const job1 = require('./jobs/job1')
 const job2 = require('./jobs/job2')
